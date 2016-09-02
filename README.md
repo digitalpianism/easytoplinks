de6d624988c17b4df162080bd4c8f92c2b776e88
Raphael Petrini <raphael@factoryx.com.au>
2016-09-02 09:55:43 +0200

    original commit

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
-# easytoplinks
+# DigitalPianism EasyToplinks
+
 A small module that provides top links management layout actions.
+
+## Features
+
+### Change position of a top link via layout XML
+
+Instead of having to remove then re-add the top link.
+
+```
+<reference name="top.links">
+        <action method="setPosition">
+        <url helper="customer/getLoginUrl"/>
+        <position>1</position>
+    </action>
+</reference>
+```
+
+The new `setPosition` method takes two parameters:
+
+ * URL of the top link
+ * New position of the top link
+
+### Rename label and title of a top link via layout XML
+
+Instead of having to remove then re-add the top link.
+
+```
+<reference name="top.links">
+    <action method="rename">
+        <url helper="customer/getLoginUrl"/>
+        <name>Log In</name>
+    </action>
+</reference>
+```
+
+The new `rename` method takes two parameters:
+
+ * URL of the top link
+ * New name of the link
+
+N.B.: with this method, both the label and the title of the top link will be renamed.
---
+++ app/code/community/DigitalPianism/EasyToplinks/Block/Page/Template/Links.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ * Class DigitalPianism_EasyToplinks_Block_Page_Template_Links
+ */
+class DigitalPianism_EasyToplinks_Block_Page_Template_Links extends Mage_Page_Block_Template_Links {
+
+    /**
+     * Change the position of an existing to make the layout customization easier
+     * @param $url
+     * @param $position
+     * @return $this
+     */
+    public function setPosition($url, $position)
+    {
+        // Get the link and delete it from the current position
+        foreach ($this->_links as $k => $v) {
+            if ($v->getUrl() == $url) {
+                $link = $this->_links[$k];
+                unset($this->_links[$k]);
+            }
+        }
+
+        if (isset($link)) {
+            // Add link to new position
+            $this->_addIntoPosition($link, $position);
+        }
+
+        return $this;
+    }
+
+    /**
+     * Rename the title and label of a top link
+     * @param $url
+     * @param $name
+     * @return $this
+     */
+    public function rename($url, $name)
+    {
+        foreach ($this->_links as $k => $v) {
+            if ($v->getUrl() == $url) {
+                // Get the link
+                $link = $this->_links[$k];
+                // Change the label and title
+                $link->setLabel($name);
+                $link->setTitle($name);
+                // Reassign the renamed link
+                $this->_links[$k] = $link;
+            }
+        }
+
+        return $this;
+    }
+}
